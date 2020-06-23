dc444ac38ac45bb38a8d741bd827bf09b10a87f1
yuu <yuudj@hotmail.com>
2020-06-23 11:51:12 -0300

    App New Version

--- version/app.js
+++ version/app.js
@@ -4,12 +4,10 @@ app = express();
 var response;
 
 app.get('/', function (req, res) {
+  response = 'This is version 2 of the app.' + '\n';
 
-    response = 'This is version 1 of the app.' + '\n';
-
-    //send the response to the client
-    res.send(response);
-
+  //send the response to the client
+  res.send(response);
 });
 
 app.listen(8080, function () {
